2c005e2ee266b700eedf6d42b6e054051d6f6331
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-06-03 17:17:11 +0200

    added a

---
+++ cinema.sql
@@ -0,0 +1,18 @@
+--a. Les informations d'un film (id_film) : titre, année, durée (au format HH:MM) et réalisateur
+
+    SELECT id_film, titre, anneeSortieFrance, DATE_FORMAT(duree, "%H:%i"), prenom, nom
+    FROM film f, personne p, realisateur re
+    WHERE f.id_realisateur = re.id_realisateur
+    AND p.id_personne = re.id_personne
+    ORDER BY titre
+
+        -- si on souhaite choisir un film en particuler :
+
+            SELECT id_film, titre, anneeSortieFrance, DATE_FORMAT(duree, "%H:%i"), prenom, nom
+            FROM film f, personne p, realisateur re
+            WHERE f.id_realisateur = re.id_realisateur
+            AND p.id_personne = re.id_personne
+            AND id_film = 1 --remplacer l'id que l'on souhaite vérifier
+    
+
+--b. Liste des films dont la durée excède 2h15, classés par durée (du + long au + court)
