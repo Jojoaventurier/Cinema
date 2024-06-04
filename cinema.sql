65d5586d1df7d934cf9cbea4b5ced8b4fa66f3c1
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-06-04 11:07:40 +0200

    updted answers

--- cinema.sql
+++ cinema.sql
@@ -23,6 +23,7 @@
     ORDER BY duree DESC
 
 --c. Liste des films d'un réalisateur (en précisant l'année de sortie)
+
     SELECT nom, titre, anneeSortieFrance
     FROM personne p, realisateur re, film f
     WHERE p.id_personne = re.id_personne
@@ -30,6 +31,7 @@
     AND nom LIKE 'Wachowschi'
 
 --d. Nombre de films par genre (classés dans l'ordre décroissant)
+   
     SELECT libelle, COUNT(fg.id_film) AS nbFilms
     FROM film f, genre g, film_genres fg
     WHERE f.id_film = fg.id_film
@@ -38,6 +40,7 @@
     ORDER BY nbFilms DESC
 
 --e. Nombre de films par réalisateur (classés dans l'ordsre décroissant)
+   
     SELECT COUNT(id_film), prenom, nom
     FROM film f, realisateur re, personne p
     WHERE f.id_realisateur = re.id_realisateur
@@ -45,6 +48,7 @@
     GROUP BY p.id_personne
 
 --f. Casting d'un film en particulier (id_film): nom, prénom des acteurs + sexe
+  
     SELECT nom, prenom, sexe, nomRole
     FROM personne p, acteur a, film f, casting c, role r
     WHERE p.id_personne = a.id_personne
@@ -55,6 +59,7 @@
 
 
 --g. Films tournés par un acteur en particulier (id_acteur)
+   
     SELECT titre, anneeSortieFrance
     FROM film f, personne p, acteur r, casting c
     WHERE f.id_film = c.id_film
@@ -63,6 +68,12 @@
     AND r.id_acteur LIKE 1
 
 --h. Liste des personnes qui sont à la fois acteurs et réalisateurs
+   
+    SELECT nom, prenom
+    FROM acteur a
+    JOIN personne p ON a.id_personne = p.id_personne
+    RIGHT JOIN realisateur re ON p.id_personne = re.id_personne
+    WHERE nom IS NOT NULL
 
 --i. Liste des films qui ont moins de 5 ans (classés du plus récent au plus ancien)
 
